ecdba09974bd1f779360c984ba0079b63b70f3e2
Snehan451 <108158059+Snehan451@users.noreply.github.com>
2023-02-24 01:53:18 +0000

    moved to training folder

--- training/main.py
+++ training/main.py
@@ -1,2 +1,6 @@
-
-print("hello world")
+a=["Twinkle, twinkle, little star",
+	    "How I wonder what you are!",
+		    "Up above the world so high,",
+		    "Like a diamond in the sky. ",
+      "Twinkle, twinkle, little star,",
+    "How I wonder what you are"]
