8b72ae278659b3c39ebd8d8acc187faec0bc6ee1
Snehan451 <108158059+Snehan451@users.noreply.github.com>
2023-03-18 00:12:39 +0000

    pyspark files

---
+++ training/studentinfo.py
@@ -0,0 +1,7 @@
+sno=int(input("Enter the student Id"))
+sname=input("Enter the student name")
+sub1=int(input("Enter the student marks"))
+sub2=int(input("Enter the student marks"))
+sub3=int(input("Enter the student marks"))
+total=sub1+sub2+sub3
+print("Totalmarks of the student is ",total)
